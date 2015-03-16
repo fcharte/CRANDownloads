5e68307a7dbd4da766246ed6a211bcd123524dd0
Francisco Charte Ojeda <francisco@fcharte.com>
2015-03-16 09:04:46 +0100

    Script to plot mldr downloads from CRAN

---
+++ CRANDownloads.R
@@ -0,0 +1,10 @@
+###### - mldr package downloads
+
+library(cranlogs)
+dwmldr <- cran_downloads(when = "last-month", package = "mldr")
+data <- dwmldr$downloads[[1]]
+
+plot(data$downloads, type = "b", lwd = 3, lty = 1, pch = 7,
+     main = "CRAN downloads for package mldr", xlab = "Date", sub = paste("sum = ", sum(data$downloads), ", mean =", mean(data$downloads)),
+     ylab = "Number of downloads", xaxt = "n")
+axis(1, at = 1:length(data$downloads), labels = data$day, cex.axis = 0.5, las = 2)
